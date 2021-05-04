5b94802653901ec07486e44df0695790f6b6e384
myxahev <76581169+myxahev@users.noreply.github.com>
2021-05-04 08:17:49 +0500

    Create Home_work_lesson-7.sql

---
+++ Home_work_lesson-7.sql
@@ -0,0 +1,78 @@
+--Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
+
+SELECT 
+	u.id AS user_id, u.name AS user_name,
+	op.order_id AS order_id,
+	op.product_id AS product_id,
+	(SELECT name FROM products WHERE id = op.product_id) AS product_name,
+	op.total
+FROM 
+	users AS u
+RIGHT JOIN
+	orders AS o 
+ON
+	u.id = o.user_id
+RIGHT JOIN
+	orders_products AS op
+ON
+	o.id = op.order_id
+ORDER BY u.name, op.order_id;
+
+--Выведите список товаров products и разделов catalogs, который соответствует товару.
+
+SELECT 
+	p.id, p.name, p.price,
+	c.id AS cat_id,
+	c.name AS catalog
+FROM
+	products AS p
+JOIN
+	catalogs AS c
+ON 
+	p.catalog_id = c.id; 
+
+--(по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). Поля from, to и label содержат английские названия городов, поле name — русское. Выведите список рейсов flights с русскими названиями городов.
+
+CREATE TABLE IF NOT EXISTS flights(
+	id SERIAL PRIMARY KEY,
+ 	`from` VARCHAR(50) NOT NULL COMMENT 'en', 
+ 	`to` VARCHAR(50) NOT NULL COMMENT 'en'
+);
+
+CREATE TABLE  IF NOT EXISTS cities(
+	label VARCHAR(50) PRIMARY KEY COMMENT 'en', 
+	name VARCHAR(50) COMMENT 'ru'
+);
+
+ALTER TABLE flights
+ADD CONSTRAINT fk_from_label
+FOREIGN KEY(`from`)
+REFERENCES cities(label);
+
+ALTER TABLE flights
+ADD CONSTRAINT fk_to_label
+FOREIGN KEY(`to`)
+REFERENCES cities(label);
+
+INSERT INTO cities VALUES
+	('Moscow', 'Москва'),
+	('Saint Petersburg', 'Санкт-Петербург'),
+ 	('Omsk', 'Омск'),
+ 	('Tomsk', 'Томск'),
+ 	('Ufa', 'Уфа');
+
+INSERT INTO flights VALUES
+ 	(NULL, 'Moscow', 'Saint Petersburg'),
+	(NULL, 'Saint Petersburg', 'Omsk'),
+ 	(NULL, 'Omsk', 'Tomsk'),
+ 	(NULL, 'Tomsk', 'Ufa'),
+ 	(NULL, 'Ufa', 'Moscow');
+
+SELECT
+	id AS flight_id,
+	(SELECT name FROM cities WHERE label = `from`) AS `from`,
+	(SELECT name FROM cities WHERE label = `to`) AS `to`
+FROM
+	flights
+ORDER BY
+	flight_id;
